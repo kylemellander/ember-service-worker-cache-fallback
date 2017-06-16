563d01b92ac8c32c37f10bf402cb5975a27db945
Kyle Mellander <kmellander@focusvision.com>
2017-06-16 13:33:41 -0700

    Update naming in build file

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ var mergeTrees = require('broccoli-merge-trees');
 var LocalForagePlugin = require('./lib/localforage');
 
 module.exports = {
-  name: 'ember-service-worker-cache-fallback',
+  name: 'ember-service-worker-smart-jsonapi-caching',
 
   included(app) {
     this._super.included && this._super.included.apply(this, arguments);
     this.app = app;
     this.app.options = this.app.options || {};
-    this.app.options['esw-cache-fallback'] = this.app.options['esw-cache-fallback'] || {};
+    this.app.options['esw-smart-jsonapi-caching'] = this.app.options['esw-smart-jsonapi-caching'] || {};
   },
 
   isDevelopingAddon: function() {
@@ -20,7 +20,7 @@ module.exports = {
   },
 
   treeForServiceWorker(swTree, appTree) {
-    var options = this.app.options['esw-cache-fallback'];
+    var options = this.app.options['esw-smart-jsonapi-caching'];
     var configFile = new Config([appTree], options);
     var localforageFile = new LocalForagePlugin([appTree], options);
 
