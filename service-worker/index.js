23e4329987ba761346be224f2e27e64b5c5289c0
Kyle Mellander <kmellander@focusvision.com>
2017-06-16 10:15:19 -0700

    import localforage

--- service-worker/index.js
+++ service-worker/index.js
@@ -1,8 +1,9 @@
 import { PATTERNS, VERSION } from 'ember-service-worker-cache-fallback/service-worker/config';
+import 'ember-service-worker-cache-fallback/service-worker/localforage';
 import cleanupCaches from 'ember-service-worker/service-worker/cleanup-caches';
 import { createUrlRegEx, urlMatchesAnyPattern } from 'ember-service-worker/service-worker/url-utils';
 
-const CACHE_KEY_PREFIX = 'esw-cache-fallback';
+const CACHE_KEY_PREFIX = 'esw-cache-fallback-localforage';
 const CACHE_NAME = `${CACHE_KEY_PREFIX}-${VERSION}`;
 
 const PATTERN_REGEX = PATTERNS.map(createUrlRegEx);
