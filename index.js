15c61628d4dcb2382c0114a9ac473356fbdc684a
Kyle Mellander <kmellander@focusvision.com>
2017-06-19 13:23:14 -0700

    [RE-4393] Clean up index.js

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-/* jshint node: true */
-'use strict';
+'use strict'
 
-var Config = require('./lib/config');
-var mergeTrees = require('broccoli-merge-trees');
-var LocalForagePlugin = require('./lib/localforage');
+var Config = require('./lib/config')
+var mergeTrees = require('broccoli-merge-trees')
+var LocalForagePlugin = require('./lib/localforage')
 
 module.exports = {
   name: 'ember-service-worker-smart-jsonapi-caching',
 
   included(app) {
-    this._super.included && this._super.included.apply(this, arguments);
-    this.app = app;
-    this.app.options = this.app.options || {};
-    this.app.options['esw-smart-jsonapi-caching'] = this.app.options['esw-smart-jsonapi-caching'] || {};
+    this._super.included && this._super.included.apply(this, arguments)
+    this.app = app
+    this.app.options = this.app.options || {}
+    this.app.options['esw-smart-jsonapi-caching'] =
+      this.app.options['esw-smart-jsonapi-caching'] || {}
+
+    this.app.options['esw-smart-jsonapi-caching'].nodeModulesPath =
+      this.nodeModulesPath
   },
 
   isDevelopingAddon: function() {
-    return true;
+    return true
   },
 
   treeForServiceWorker(swTree, appTree) {
-    var options = this.app.options['esw-smart-jsonapi-caching'];
-    var configFile = new Config([appTree], options);
-    var localforageFile = new LocalForagePlugin([appTree], options);
+    var options = this.app.options['esw-smart-jsonapi-caching']
+    var configFile = new Config([appTree], options)
+    var localforageFile = new LocalForagePlugin([appTree], options)
 
-    return mergeTrees([swTree, configFile, localforageFile]);
+    return mergeTrees([swTree, configFile, localforageFile])
   }
-};
+}
